822fa179113b4e4f20feae6c8766921ae97e4154
Priyanka20111999 <priyankanayak20111999@gmail.com>
2023-06-19 10:46:39 +0530

    remove drop down of blog Menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2e310f85b7eb449"; }
+/******/ 		__webpack_require__.h = function() { return "d13871cd81813552"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
